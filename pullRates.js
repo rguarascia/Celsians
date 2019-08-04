20b14e1280f1096bc4be3d85d99f52c5c4289765
Ryan Guarascia <rguarascia@gmail.com>
2019-08-03 23:15:21 -0400

    Added much better inserting and updating.
    (Actually works.)

--- pullRates.js
+++ pullRates.js
@@ -1,10 +1,17 @@
+// Ryan Guarascia
+// Aug 1st 2019
+
 import { fetch } from 'wix-fetch';
 import wixData from 'wix-data';
 
 var newRates = {};
 
+var errorList = "";
+
 export function getdata() {
 	console.log("API CALLED");
+
+	var dbData = [];
 
 	// Gets data from API
 	fetch("https://wallet-api.celsius.network/util/interest/rates", { "method": "get" })
@@ -14,7 +21,6 @@ export function getdata() {
 			}
 		})
 		.then(json => {
-			console.log("Parsing json");
 			newRates = json;
 			console.log(newRates);
 			// Runs a query to get the current data
@@ -22,52 +28,60 @@ export function getdata() {
 				.find()
 				.then((results) => {
 					// go through all new rates
+					var skipList = [];
 					for (var x = 0; x < newRates.interestRates.length; x++) {
-						var found = false;
-						var coinChange = "";
-						loop1:
-							for (var y = 0; x < results.items.length; y++) {
-								if (results.items[y] !== null && newRates.interestRates[x] !== null) {
-									// if the coin is already in the db
-									if (newRates.interestRates[x].coin === results.items[y].coin) {
-										found = true;
-										coinChange = results.items[y]._id;
-										console.log(coinChange);
-										break loop1;
-									}
-								}
+						var updatedRate = newRates.interestRates[x].rate;
+						// checks if the db has the current coin
+						var flag = results.items.filter(y => y.coin === newRates.interestRates[x].coin);
+						if (flag.length !== 0) { // if it is found
+							console.log("Checking: " + newRates.interestRates[x].coin + " w/ rate: " + newRates.interestRates[x].rate);
+							console.log("current ID: " + flag[0]._id);
+							console.log("Exisiting coin. Checking for update.");
+							if (flag[0].rate !== newRates.interestRates[x].rate) {
+								console.log("New rate found. Updating");
+								updateThis(flag, newRates.interestRates[x].rate);
+							} else {
+								console.log("Rate is the same. Disregard");
 							}
-						if (found) {
-							console.log("Coin already exist. Updating");
-							wixData.get("rates", coinChange)
-								.then((item) => {
-									item.rate = newRates.interestRates[x].rate; // updated new rate
-									wixData.update("rates", item);
-								})
-								.catch((err) => {
-									let errorMsg = err;
-								});
-						} else { // if it is a new coin
-							console.log("Coin is new, adding");
-							let toInsert = {
-								"coin": newRates.interestRates[x].coin,
-								"rate": newRates.interestRates[x].rate,
-								"name": newRates.interestRates[x].currency.name,
-								"url": newRates.interestRates[x].currency.image_url
-							};
-							wixData.insert("rates", toInsert)
-								.then((results2) => {
-									let item = results2; // intsert below
-								})
-								.catch((err) => {
-									let errorMsg = err;
-								});
+						} else {
+							console.log("New coin. Adding: " + newRates.interestRates[x].coin + " to db");
+							insertThis(newRates.interestRates[x]);
 						}
 					}
-					console.log(results.items);
 				});
-
-			return "Added new rates";
 		})
 		.catch(err => console.log(err));
+	return "Updated";
+}
+
+function updateThis(currentCoin, updateRate) {
+	wixData.get("rates", currentCoin[0]._id)
+		.then((item) => {
+			console.log("found: " + currentCoin[0].coin);
+			console.log("New Rate: " + updateRate);
+			item.rate = updateRate; // updated w/ new rate
+			item.CELrate = (updateRate*1.3).toFixed(4).toString()
+			wixData.update("rates", item);
+		})
+		.catch(err => {
+			let errorMsg = err;
+			console.log("error: " + err);
+		});
+}
+
+function insertThis(newCoin) {
+	let toInsert = {
+		"coin": newCoin.coin,
+		"rate": newCoin.rate,
+		"CELrate": (newCoin.rate*1.3).toFixed(4).toString(),
+		"name": newCoin.currency.name,
+		"url": newCoin.currency.image_url,
+	};
+	wixData.insert("rates", toInsert)
+		.then((results2) => {
+			let item = results2; // insert below
+		})
+		.catch((err) => {
+			let errorMsg = err;
+		});
 }
